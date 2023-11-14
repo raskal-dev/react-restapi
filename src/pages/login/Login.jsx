80aa4e12cc01b365bf620247a7cb5899b9b7683b
Kalvin RASAMY <102739808+raskal-dev@users.noreply.github.com>
2023-11-14 22:31:09 +0300

    test

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,9 @@ const Login = () => {
     return ( 
         <>
             <h1>Login</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure amet magnam pariatur doloribus quas. Sit pariatur optio nisi laborum ratione ipsam sed reprehenderit, consequatur, veritatis, officia minus non inventore hic quod quasi corporis asperiores ducimus deleniti accusamus rerum doloribus. Aspernatur?
+            </p>
         </>
      );
 }
