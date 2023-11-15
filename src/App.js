42789f3911842200764d440ad49805dd7d3b125e
Kalvin RASAMY <102739808+raskal-dev@users.noreply.github.com>
2023-11-15 20:24:27 +0300

    Menu *

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login/Login';
+import Menu from './pages/menu/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Routes>
         <Route path='/' element={<Login />} />
       </Routes>
