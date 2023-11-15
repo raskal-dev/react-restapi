2fe6226ae713b1ce68a9d27029d14c0652018110
Kalvin RASAMY <102739808+raskal-dev@users.noreply.github.com>
2023-11-15 20:23:27 +0300

    Menu

---
+++ src/pages/menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <>
+            <li>Accueil</li>
+            <li>Lists User</li>
+        </>
+    );
+}
+
+export default Menu;
