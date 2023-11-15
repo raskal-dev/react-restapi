e537987ffe5d9c02ea2bdfaac3580c9f3562e7ec
Kalvin RASAMY <102739808+raskal-dev@users.noreply.github.com>
2023-11-15 21:46:55 +0300

    css menu 1

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Menu from "../menu/Menu";
 
 const Login = () => {
     return ( 
         <div className="Login">
+            <Menu />
             <h1>Login</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure amet magnam pariatur doloribus quas. Sit pariatur optio nisi laborum ratione ipsam sed reprehenderit, consequatur, veritatis, officia minus non inventore hic quod quasi corporis asperiores ducimus deleniti accusamus rerum doloribus. Aspernatur?
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
+    const [activeLink, setActiveLink] = useState('')
+
+    useEffect(() => {
+        setActiveLink(window.location.pathname)
+    }, [])
+
+    const handleClicl = (link) => {
+        setActiveLink(link)
+    }
+
     return (
         <div className="Menu">
             <nav className="navbar">
 
-                <Link 
-                to={'/'}
-                aria-placeholder="Home"
-                />
+                <ul>
+                    <li className={`list $(activeLink === '/' ? 'active' : '')`}>
+                        <Link
+                            aria-current="page"
+                            to="/"
+                            onClick={() => handleClicl('/')}
+                        >
+                            <span className="text-label">Home</span>
+                        </Link>
+                    </li>
+                    <li>
+
+                    </li>
+                    <li>
+
+                    </li>
+                    <li>
+                        
+                    </li>
+                </ul>
 
             </nav>
         </div>
