1023ddd8c9264dabbb5e6bf2c4b22fd6f8a2b419
Kalvin RASAMY <102739808+raskal-dev@users.noreply.github.com>
2023-11-15 20:30:58 +0300

    css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const Login = () => {
     return ( 
-        <>
+        <div className="Login">
             <h1>Login</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure amet magnam pariatur doloribus quas. Sit pariatur optio nisi laborum ratione ipsam sed reprehenderit, consequatur, veritatis, officia minus non inventore hic quod quasi corporis asperiores ducimus deleniti accusamus rerum doloribus. Aspernatur?
             </p>
-        </>
+            <p>
+                Mandea
+            </p>
+        </div>
      );
 }
 
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Menu = () => {
     return (
-        <>
+        <div className="Menu">
             <li>Accueil</li>
             <li>Lists User</li>
-        </>
+        </div>
     );
 }
 
